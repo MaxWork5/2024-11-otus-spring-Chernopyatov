c9259d9286b53fe3c7b75c833691f62b5a86d51e
Maxim Chernopyatov <max_chernopyatov@mail.ru>
2025-02-28 22:40:46 +0300

    Библиотеку на Spring Data JPA
    1. Переписать все репозитории по работе с книгами на Spring Data JPA репозитории.
    
    2. Используйте spring-boot-starter-data-jpa.
    
    3. Кастомные методы репозиториев (или с хитрым @Query) покрыть тестами, используя H2.
    
    4. @Transactional рекомендуется ставить на методы сервисов, а не репозиториев.

---
+++ hw07/src/main/java/ru/otus/hw/models/Comment.java
@@ -0,0 +1,28 @@
+package ru.otus.hw.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "book_id")
+    private long bookId;
+}
