1e715fc4b7198b6644bc7a77b3ac6eb5020b1f28
Maxim Chernopyatov <max_chernopyatov@mail.ru>
2025-03-21 21:38:19 +0300

    Приложение с использованием AJAX и REST-контроллеров
    1. Переписать приложение с классических View на AJAX архитектуру и REST-контроллеры;
    
    2. Минимум: получение одной сущности и отображение её на странице с помощью XmlHttpRequest, fetch api или jQuery;
    
    3. Протестировать все эндпойнты REST-контроллеров с моками зависимостей;

---
+++ hw10/src/test/java/ru/otus/hw/repositories/JpaGenreRepositoryTest.java
@@ -0,0 +1,35 @@
+package ru.otus.hw.repositories;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import ru.otus.hw.repositories.GenreRepository;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Репозиторий на основе JPA для работы с жанрами")
+@DataJpaTest
+class JpaGenreRepositoryTest {
+
+    @Autowired
+    private GenreRepository repository;
+
+    @DisplayName("должен загружать список всех жанров")
+    @Test
+    void findAll() {
+        var genre = repository.findAll();
+        assertThat(genre).isNotNull().hasSize(6)
+                .allMatch(s -> !s.getName().isEmpty());
+    }
+
+    @DisplayName("должен загрузить жанр по принадлежащему ему идентификатору")
+    @Test
+    void findAllByIds() {
+        var genre = repository.findAllByIdIn(Set.of(1L, 2L, 3L));
+        assertThat(genre).isNotNull().hasSize(3)
+                .allMatch(s -> !s.getName().isEmpty());
+    }
+}
